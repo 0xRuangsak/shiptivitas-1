d3d81578e817fcf40b63fd8d1c489fdba6d02c46
0xAstin <astinashler@gmail.com>
2024-07-03 12:02:34 +0700

    ensure  each swimlane has class "Swimlane-column"

--- src/Swimlane.js
+++ src/Swimlane.js
@@ -2,6 +2,35 @@ import React from 'react';
 import Card from './Card';
 import './Swimlane.css';
 
+function Swimlane({ swimlane, tasks, onDragEnd }) {
+  return (
+    <div className="Swimlane-column">
+      <h3>{swimlane.title}</h3>
+      <Droppable droppableId={swimlane.id}>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {tasks.map((task, index) => (
+              <Draggable key={task.id} draggableId={task.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <Card task={task} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+}
+
+
 export default class Swimlane extends React.Component {
   render() {
     const cards = this.props.clients.map(client => {
